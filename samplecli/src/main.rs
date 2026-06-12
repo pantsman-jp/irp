58b5cdf8fb9c615aae62d0f84d573dd855892107
pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-06-12 12:14:15 +0900

    section4.1

---
+++ samplecli/src/main.rs
@@ -0,0 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "pantsman",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    #[arg(short, long)]
+    verbose: bool,
+
+    #[arg(name = "FILE")]
+    formula_file: Option<String>,
+}
+
+fn main() {
+    let opts = Opts::parse();
+    match opts.formula_file {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+    println!("Is verbosity specified?: {}", opts.verbose);
+}
